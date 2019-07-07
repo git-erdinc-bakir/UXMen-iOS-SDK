3536fe3e8181f35f82b5ce0d26e34eef7478a836
E_BAKIR <e.bakir@thy.com>
2019-07-07 15:17:17 +0300

    POD SAYS HELLO

---
+++ UXMenSDK.h
@@ -0,0 +1,18 @@
+//
+//  UXMenSDK.h
+//  Pods
+//
+//  Created by E_BAKIR on 7.07.2019.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UXMenSDK : NSObject
+
+- (void)sayHello;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UXMenSDK.m
@@ -0,0 +1,18 @@
+//
+//  UXMenSDK.m
+//  Pods
+//
+//  Created by E_BAKIR on 7.07.2019.
+//
+
+#import "UXMenSDK.h"
+
+@implementation UXMenSDK
+
+- (void)sayHello {
+    
+    NSLog(@"UXMen says hello");
+    
+}
+
+@end
